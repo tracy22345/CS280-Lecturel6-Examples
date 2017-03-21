b58223a25a32c04995a61b4b56e394cda26b4be2
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-21 21:01:38 +0800

    練習繼承

---
+++ PokemonLibrary/GrassTypePokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class GrassTypePokemon : Pokemon
+    {
+        public int GrassTypePowerUpCandy { get; set; }
+
+        public int GrassTypePowerUpStarDust { get; set; }
+
+        public GrassTypePokemon(
+            string name,
+            int nationalNo,
+            string[] type,
+            float height,
+            float weight,
+            int grassTypePowerUpCandy,
+            int grassTypePowerUpStarDust
+        )
+            : base(
+                  name,
+                  nationalNo,
+                  type,
+                  height,
+                  weight
+                 )
+        {
+            this.GrassTypePowerUpCandy = grassTypePowerUpCandy;
+            this.GrassTypePowerUpStarDust = grassTypePowerUpStarDust;
+        }
+    }
+}
---
+++ PokemonLibrary/Pokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+
+        public int NationalNo { get; set; }
+
+        public string[] Type { get; set; }
+
+        public float Height { get; set; }
+
+        public float Weight { get; set; }
+
+        public Pokemon(
+            string name,
+            int nationalNo,
+            string[] type,
+            float height,
+            float weight
+            )
+        {
+            this.Name = name;
+            this.NationalNo = nationalNo;
+            this.Type = type;
+            this.Height = height;
+            this.Weight = weight;
+        }
+    }
+}
